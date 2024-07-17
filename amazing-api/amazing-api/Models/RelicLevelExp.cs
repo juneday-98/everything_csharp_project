cc43ca10ec87733a670023bece8a1beb3d23e648
juneday-98 <preawpan.dev@gmail.com>
2024-07-17 16:47:53 +0700

    Add new Entity -> RelicLevelExp

---
+++ amazing-api/amazing-api/Models/RelicLevelExp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace amazing_api.Models;
+
+public partial class RelicLevelExp
+{
+    public int Id { get; set; }
+
+    public int RelicRarityId { get; set; }
+
+    public int CurrentLevel { get; set; }
+
+    public int Exp { get; set; }
+
+    public DateTime? CreateDate { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+}
