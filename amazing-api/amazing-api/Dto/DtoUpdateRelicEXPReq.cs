39733d1ab0cbb82538d8abe7079bed50d007f9e3
juneday-98 <preawpan.dev@gmail.com>
2024-07-24 22:02:08 +0700

    Add Relic DTO for update exp relic

---
+++ amazing-api/amazing-api/Dto/DtoUpdateRelicEXPReq.cs
@@ -0,0 +1,24 @@
+﻿using amazing_api.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace amazing_api.Dto
+{
+    public class DtoUpdateRelicEXPReq
+    {
+
+        public DtoUpdateRelicEXPReq(Relic inputRelic, int inputExp)
+        {
+            this.CurrentRelice = inputRelic;
+            this.exp = inputExp;
+        }
+        
+        [Required(ErrorMessage = "Relic is required.")]
+        public Relic CurrentRelice { get; set; }
+        
+        
+        [Required(ErrorMessage = "Exp amount is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Exp must be a positive integer.")]
+        public int exp { get; set; }
+
+    }
+}
